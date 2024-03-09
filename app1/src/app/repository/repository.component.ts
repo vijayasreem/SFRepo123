f3738818321edae5f34bc3d8ab06d3a76c7fe4dc
dotnet-code <@dotnet-code>
2024-03-09 05:54:01 +0000

    commit from code202403095402

---
+++ app1/src/app/repository/repository.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-repository',
+  templateUrl: './repository.component.html',
+  styleUrls: ['./repository.component.css']
+})
+export class RepositoryComponent {
+
+}
